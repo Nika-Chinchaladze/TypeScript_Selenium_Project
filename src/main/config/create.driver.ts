cbadeda932144c9930e6e7e661430bd9c990ff4d
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2025-07-13 17:55:50 +0400

    ci/cd implemented

--- src/main/config/create.driver.ts
+++ src/main/config/create.driver.ts
@@ -3,9 +3,21 @@ import * as chrome from 'selenium-webdriver/chrome';
 import * as chromedriver from 'chromedriver';
 
 export class CreateDriver {
+  getBrowserOptions(): chrome.Options {
+    const options = new chrome.Options();
+    options.addArguments('--headless=new');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--window-size=1920,1080');
+    return options;
+  }
+
   async createChromeDriver(): Promise<WebDriver> {
     const service = new chrome.ServiceBuilder(chromedriver.path);
-    const driver = await new Builder().forBrowser(Browser.CHROME).setChromeService(service).build();
+    const driver = await new Builder()
+      .forBrowser(Browser.CHROME)
+      .setChromeService(service)
+      .setChromeOptions(this.getBrowserOptions())
+      .build();
     await driver.manage().window().maximize();
     return driver;
   }
